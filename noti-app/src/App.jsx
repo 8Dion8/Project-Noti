de44c9f49a0cdaf174efd6e1d445fcbafdb11632
8Dion8 <shvartserinfo@gmail.com>
2023-04-21 22:41:18 +0300

    solid web app beginnings

---
+++ noti-app/src/App.jsx
@@ -0,0 +1,17 @@
+import { Tab } from "@hope-ui/solid";
+import MenuDrawer from "./components/Menu"
+import TableMain from "./components/Table"
+
+function App() {
+
+
+  return (
+    <>
+    <MenuDrawer />
+    <TableMain />
+    </>
+    
+  );
+}
+
+export default App;
